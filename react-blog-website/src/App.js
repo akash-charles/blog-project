a5b92db1a3280a1a9bf4dba1a404d09a59279263
akash-charles <akash262003@gmail.com>
2023-10-15 18:31:38 +0530

    Login and styles

--- react-blog-website/src/App.js
+++ react-blog-website/src/App.js
@@ -1,9 +1,68 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+import Register from "./Pages/Register"
+import Login from "./Pages/Login"
+import Single from "./Pages/Single"
+import Write from "./Pages/Write"
+import Home from "./Pages/Home"
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar"
+import "./Styles.scss"
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/post/:id",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      }
+    ]
+  },
+  {
+    path: "/Register",
+    element: <Register />,
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+]);
+
+
 function App() {
   return (
-    <div className="App">
-      yo
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router}/>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
